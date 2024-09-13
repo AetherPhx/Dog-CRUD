d3c6dd9c8df27ecc071659079407c287a114c4a3
AetherPhx <aetherphx@gmail.com>
2024-09-13 13:53:29 -0500

    Create and handle a dinamyc modal to delete or add dogs

--- scripts/modalHandler.js
+++ scripts/modalHandler.js
@@ -1,65 +1,144 @@
-// Modal dinámico que su contenido sea de formulario o de confirmación.
-// * Tipos de modales: Formulario & Confirmación.
-// En el caso de formulario el contenido es un formulario con los campos de datos de la mascota y se debe realizar una validación al presionar en aceptar.
-// En el caso de confirmación se muestra una pregunta para confirmar la acción que se quiere realizar.
-// En ambos casos, si todo es correcto, se debe realizar la acción y cerrar el modal.
+import { deleteDog, addDog } from "./crud";
+let overlay;
+class Modal {
+	constructor(action, dog) {
+		this.actionName = action;
+		this.dog = dog;
+		this.modalHTML = null;
+		this.modalForm = null;
 
-// * Ciclo de vida del modal:
-// Al querer agregar, editar o eliminar una mascota, se generará el modal y una vez que haya éxito, el modal-overlay se agrega al DOM y se superpone sobre el contenido de la página.
-// Al momento de crear el modal, se debe asignar una acción que se ejecutará cuando se presione en aceptar y que se cancelará cuando se presione en cancelar, cuando se cierre el modal (Presionando en la X) o cuando se presione fuera del modal (también cierra el modal).
-// Para generar el modal, primero se debe crear el contenido del modal respectivo. Una vez creado el contenido, se mostrará tanto el overlay como el modal a espera de que se presione en aceptar o cancelar.
+		this.setUpModal();
+	}
 
-// * Desarrollo:
-// El modal se llama cuando se quiere agregar, editar o eliminar una mascota. Por lo tanto, se debe crear una función que reciba como parámetro la acción que se desea realizar.
-// Esta fn llamará otra fn que se encargará de preparar el modal en base a la acción que se desee realizar. Por último, se agregará el overlay y modal al DOM.
-// Para preparar el modal, se creará su contenido HTML respectivo y agregará la fn crud relacionada. También se asignarán los listeners necesarios para que se puedan ejecutar o cancelar la acción llamando a la fn que cierra el modal (Cambia la clase del overlay a modal-overlay--hidden).
-// Una vez que la fn de preparar el modal haya sido un éxito, se llamará otra fn que abra el modal. O sea, que le brindará la clase modal-overlay al elemento del DOM.
+	setUpModal() {
+		this.createModalHTML();
+		this.handleEvents();
+	}
+
+	// Maquetación del modal
+	createModalHTML() {
+		this.modalHTML = document.createElement("section");
+		this.modalHTML.className = "modal";
+		this.modalHTML.appendChild(this.createModalMenu());
+		this.modalHTML.appendChild(this.createModalContent());
+		this.modalHTML.appendChild(this.createModalActions());
+		overlay.appendChild(this.modalHTML);
+	}
+	createModalMenu() {
+		const modalMenu = document.createElement("header");
+		modalMenu.className = "modal-menu";
+		modalMenu.innerHTML = `<button class="modal-close">X</button>`;
+		if (this.actionName === "add")
+			modalMenu.innerHTML += `<h3 class="modal-title">Agregar Mascota</h3>`;
+		else if (this.actionName === "edit")
+			modalMenu.innerHTML += `<h3 class="modal-title">Editar Mascota</h3>`;
+
+		return modalMenu;
+	}
+	createModalContent() {
+		if (this.actionName === "delete") {
+			const modalContent = document.createElement("p");
+			modalContent.className = "modal-question";
+			modalContent.textContent = `¿Estas seguro de querer eliminar a este perro?`;
+			return modalContent;
+		} else if (this.actionName === "add") {
+			const modalContent = document.createElement("form");
+			modalContent.className = "modal-form";
+			modalContent.innerHTML = `
+				<input class="modal-input" type="text" name="name" placeholder="Nombre" required>
+				<input class="modal-input" type="text" name="img" placeholder="URL de la imagen" required>
+				<input class="modal-input" type="text" name="phone" placeholder="Teléfono" required>
+				<input class="modal-input" type="email" name="mail" placeholder="Email" required>
+				<input class="modal-input" type="text" name="country" placeholder="País" required>
+				<textarea name="description" placeholder="Descripción" required></textarea>
+			`;
+			this.modalForm = modalContent;
+			return modalContent;
+		}
+	}
+	createModalActions() {
+		const modalActions = document.createElement("footer");
+		modalActions.className = "modal-actions";
+		if (this.actionName === "delete")
+			modalActions.innerHTML = `
+			<button class="modal-confirm">Aceptar</button>`;
+		else if (this.actionName === "add")
+			modalActions.innerHTML = `
+        	<button type="submit" class="modal-confirm">Aceptar</button>`;
+
+		modalActions.innerHTML += `<button class="modal-cancel">Cancelar</button>`;
+		return modalActions;
+	}
+
+	handleEvents() {
+		this.modalHTML.addEventListener("click", (event) => {
+			const { target } = event;
+			if (target.classList.contains("modal-confirm")) this.action();
+		});
+	}
+
+	// Asignación CRUD
+	action() {
+		if (this.actionName === "delete") deleteDog(this.dog);
+		else if (this.actionName === "add") addDog(this.modalForm);
+		else if (this.actionName === "edit") null; // TODO: To be implemented
+	}
+}
 
 function modalHandler(action, dog) {
-	console.log("Función modalHandler");
-	console.log(action, dog);
+	console.group("Se ha llamado a la función modalHandler");
+	console.log("Los parámetros son:", action, dog);
 
+	prepareOverlay();
 	prepareModal(action, dog);
 	openModal();
 }
 
-function prepareModal(action, dog) {
-	console.log("Preparando modal");
-	const modal = document.createElement("section");
-	modal.className = "modal";
-	modal.innerHTML = `
-        <div class="modal-menu">
-            <h3 class="modal-title">Confirmar</h3>
-            <button class="modal-close">X</button>
-        </div>
-    
-        <section class="modal-content">
-            <p class="modal-question">
-                ¿Estas seguro de querer ${action} a este perro?
-            </p>
-        </section>
+function prepareOverlay() {
+	if (document.getElementById("modal-overlay")) return false;
 
-        <footer class="modal-actions">
-            <button class="modal-confirm">Aceptar</button>
-            <button class="modal-cancel">Cancelar</button>
-        </footer>
-        `;
-	document.getElementById("modal-overlay").appendChild(modal);
+	console.log("Preparando Overlay...");
+	overlay = document.createElement("div");
+	overlay.id = "modal-overlay";
+	overlay.classList = "modal-overlay--hidden";
+
+	overlay.addEventListener("click", (event) => {
+		const { target } = event;
+
+		if (
+			target.classList.contains("modal-overlay") ||
+			target.classList.contains("modal-close") ||
+			target.classList.contains("modal-cancel")
+		) {
+			console.log("Cancelar acción");
+			closeModal();
+		}
+	});
+
+	document.querySelector(".layout").appendChild(overlay);
+	console.log("Overlay preparado");
+	return true;
+}
+
+function prepareModal(action, dog) {
+	console.log("Preparando modal...");
+	const modal = new Modal(action, dog);
+	overlay.appendChild(modal.modalHTML);
+	console.log(modal.modalHTML);
+	console.log("Modal preparado!");
 }
 
 function openModal() {
-	document.getElementById("modal-overlay").classList = "modal-overlay";
+	overlay.classList = "modal-overlay";
 	console.log("Modal abierto");
-
-	// ! Only to test closeModal():
-	setTimeout(() => {
-		closeModal();
-	}, 5000);
 }
 
-function closeModal() {
-	document.getElementById("modal-overlay").classList = "modal-overlay--hidden";
+export function closeModal() {
+	overlay.classList = "modal-overlay--hidden";
+	if (document.querySelector(".modal"))
+		document.querySelector(".modal").remove();
 	console.log("Modal cerrado");
+	console.groupEnd();
 }
 
 export { modalHandler };
